e910c882d1f7353ccd52ee9f1e2328f56072cf6e
grupo5-2025 <grupo5.poo@gmail.com>
2025-05-10 15:50:43 -0500

    Update GUI.java

--- Colegio/src/Program/GUI.java
+++ Colegio/src/Program/GUI.java
@@ -18,8 +18,6 @@ public class GUI extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JLabel lblNewLabel;
-	private JTextField txtcode;
 	private JLabel lblApellidosYNombres;
 	private JTextField txtname;
 	private JLabel lblNotaN;
@@ -30,9 +28,6 @@ public class GUI extends JFrame implements ActionListener {
 	private JTextField txtnota2;
 	private JTextField txtnota3;
 	private JTextField txtnota4;
-	private JButton btnNewButton;
-	private JButton btnBuscar;
-	private JButton btnModificar;
 	private JButton btnEliminar;
 	private JScrollPane scrollPane;
 	private JTextArea txtS;
@@ -58,124 +53,158 @@ public class GUI extends JFrame implements ActionListener {
 	 */
 	public GUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 781, 531);
+		setBounds(100, 100, 784, 656);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		{
-			lblNewLabel = new JLabel("Número de lista : ");
-			lblNewLabel.setBounds(27, 41, 125, 13);
-			contentPane.add(lblNewLabel);
-		}
-		{
-			txtcode = new JTextField();
-			txtcode.setBounds(162, 38, 96, 19);
-			contentPane.add(txtcode);
-			txtcode.setColumns(10);
-		}
-		{
 			lblApellidosYNombres = new JLabel("Apellidos y Nombres : ");
-			lblApellidosYNombres.setBounds(27, 90, 125, 13);
+			lblApellidosYNombres.setBounds(27, 41, 125, 13);
 			contentPane.add(lblApellidosYNombres);
 		}
 		{
 			txtname = new JTextField();
-			txtname.setBounds(158, 87, 296, 19);
+			txtname.setBounds(150, 37, 296, 19);
 			contentPane.add(txtname);
 			txtname.setColumns(10);
 		}
 		{
 			lblNotaN = new JLabel("Nota N° 1");
-			lblNotaN.setBounds(63, 149, 59, 13);
+			lblNotaN.setBounds(70, 104, 59, 13);
 			contentPane.add(lblNotaN);
 		}
 		{
 			lblNotaN_4 = new JLabel("Nota N° 2");
-			lblNotaN_4.setBounds(277, 149, 59, 13);
+			lblNotaN_4.setBounds(284, 104, 59, 13);
 			contentPane.add(lblNotaN_4);
 		}
 		{
 			lblNotaN_1 = new JLabel("Nota N° 3");
-			lblNotaN_1.setBounds(483, 149, 59, 13);
+			lblNotaN_1.setBounds(490, 104, 59, 13);
 			contentPane.add(lblNotaN_1);
 		}
 		{
 			lblNotaN_2 = new JLabel("Nota N° 4");
-			lblNotaN_2.setBounds(671, 149, 59, 13);
+			lblNotaN_2.setBounds(678, 104, 59, 13);
 			contentPane.add(lblNotaN_2);
 		}
 		{
 			txtnota1 = new JTextField();
-			txtnota1.setBounds(52, 172, 70, 19);
+			txtnota1.setBounds(59, 127, 70, 19);
 			contentPane.add(txtnota1);
 			txtnota1.setColumns(10);
 		}
 		{
 			txtnota2 = new JTextField();
 			txtnota2.setColumns(10);
-			txtnota2.setBounds(266, 172, 70, 19);
+			txtnota2.setBounds(273, 127, 70, 19);
 			contentPane.add(txtnota2);
 		}
 		{
 			txtnota3 = new JTextField();
 			txtnota3.setColumns(10);
-			txtnota3.setBounds(472, 172, 70, 19);
+			txtnota3.setBounds(479, 127, 70, 19);
 			contentPane.add(txtnota3);
 		}
 		{
 			txtnota4 = new JTextField();
 			txtnota4.setColumns(10);
-			txtnota4.setBounds(660, 172, 70, 19);
+			txtnota4.setBounds(667, 127, 70, 19);
 			contentPane.add(txtnota4);
 		}
 		{
-			btnNewButton = new JButton("Registar\r\n");
-			btnNewButton.addActionListener(this);
-			btnNewButton.setBounds(483, 10, 85, 21);
-			contentPane.add(btnNewButton);
-		}
-		{
-			btnBuscar = new JButton("Buscar");
-			btnBuscar.setBounds(626, 10, 85, 21);
-			contentPane.add(btnBuscar);
-		}
-		{
-			btnModificar = new JButton("Modificar");
-			btnModificar.setBounds(483, 59, 85, 21);
-			contentPane.add(btnModificar);
-		}
-		{
-			btnEliminar = new JButton("Eliminar");
-			btnEliminar.setBounds(626, 59, 85, 21);
+			btnEliminar = new JButton("Buscar");
+			btnEliminar.addActionListener(this);
+			btnEliminar.setBounds(245, 72, 85, 21);
 			contentPane.add(btnEliminar);
 		}
 		{
 			scrollPane = new JScrollPane();
-			scrollPane.setBounds(27, 207, 710, 262);
+			scrollPane.setBounds(27, 157, 710, 165);
 			contentPane.add(scrollPane);
 			{
 				txtS = new JTextArea();
 				scrollPane.setViewportView(txtS);
 			}
 		}
+		{
+			btnNewButton_1 = new JButton("CURSO #1");
+			btnNewButton_1.setBounds(481, 38, 109, 19);
+			contentPane.add(btnNewButton_1);
+		}
+		{
+			btnNewButton_2 = new JButton("CURSO #2");
+			btnNewButton_2.addActionListener(this);
+			btnNewButton_2.setBounds(600, 38, 107, 19);
+			contentPane.add(btnNewButton_2);
+		}
+		{
+			scrollPane_1 = new JScrollPane();
+			scrollPane_1.setBounds(27, 333, 710, 165);
+			contentPane.add(scrollPane_1);
+			{
+				textArea = new JTextArea();
+				scrollPane_1.setViewportView(textArea);
+			}
+		}
+		{
+			lblNewLabel_1 = new JLabel("Registrar");
+			lblNewLabel_1.setBounds(486, 20, 46, 14);
+			contentPane.add(lblNewLabel_1);
+		}
+		{
+			textField = new JTextField();
+			textField.setBounds(364, 519, 373, 75);
+			contentPane.add(textField);
+			textField.setColumns(10);
+		}
+		{
+			lblNewLabel_2 = new JLabel("Observaciones del docente:");
+			lblNewLabel_2.setBounds(224, 541, 142, 31);
+			contentPane.add(lblNewLabel_2);
+		}
+		{
+			btnNewButton = new JButton("GENERAR LIBRETA");
+			btnNewButton.addActionListener(this);
+			btnNewButton.setBounds(31, 532, 183, 49);
+			contentPane.add(btnNewButton);
+		}
+		{
+			btnEliminar_1 = new JButton("Eliminar");
+			btnEliminar_1.setBounds(150, 71, 85, 21);
+			contentPane.add(btnEliminar_1);
+		}
 	}
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnEliminar) {
+			do_btnEliminar_actionPerformed(e);
+		}
 		if (e.getSource() == btnNewButton) {
 			do_btnNewButton_actionPerformed(e);
 		}
-	}
-	arraystudent arreglo = new arraystudent();
-	protected void do_btnNewButton_actionPerformed(ActionEvent e) {
-		Imprimir("Número lista\tNombre\t\tNota 1\tNota 2\tNota 3\tNota 4\tPromedio");
-		for (int i = 0; i < arreglo.Tamaño(); i++) {
-			Imprimir(""+arreglo.Obtener(i).getCod()+"\t"+arreglo.Obtener(i).getNom()+"\t"+arreglo.Obtener(i).getN1()+
-		"\t"+arreglo.Obtener(i).getN2()+"\t"+arreglo.Obtener(i).getN3()+"\t"+arreglo.Obtener(i).getN4()+"\t"+arreglo.Obtener(i).promedio());
+		if (e.getSource() == btnNewButton_2) {
+			do_btnNewButton_2_actionPerformed(e);
 		}
 	}
+	arraystudent arreglo = new arraystudent();
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JScrollPane scrollPane_1;
+	private JLabel lblNewLabel_1;
+	private JTextArea textArea;
+	private JTextField textField;
+	private JLabel lblNewLabel_2;
+	private JButton btnNewButton;
+	private JButton btnEliminar_1;
 	void Imprimir(String s) {
 		txtS.append(s+"\n");
 	}
-
+	protected void do_btnNewButton_2_actionPerformed(ActionEvent e) {
+	}
+	protected void do_btnNewButton_actionPerformed(ActionEvent e) {
+	}
+	protected void do_btnEliminar_actionPerformed(ActionEvent e) {
+	}
 }
